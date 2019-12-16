2ae1aa6f0c1d0923d5104edb252a4998dd092d34
ParadiseSeeker <paradiseseeker94@gmail.com>
2019-12-16 04:20:07 +0200

    :/

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,13 @@ if ($('.js-slider-works').length){
 	sliderWorks()
 }
 
+$('.js-hamburger').on('click', function(e) {
+	e.preventDefault();
+	$('.js-hamburger').toggleClass('active');
+	$('.js-nav-mobile').toggleClass('active');
+	$('.js-blurry').toggleClass('blurry');
+});
+
 //test
 import domToImage from 'dom-to-image';
 import targetToShatter from './firstScreen/targetToShatter';
